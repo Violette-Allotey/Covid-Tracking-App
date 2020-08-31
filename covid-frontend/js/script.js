09733621b865f3ccd26eeebc7fcc1e6d48e3253f
Charles Allotey <niovaley@gmail.comgit config --global user.name Violette-Allotey>
2020-08-31 12:57:20 +0000

    first commit

---
+++ covid-frontend/js/script.js
@@ -0,0 +1,106 @@
+let baseUrl = 'http://localhost:5000'
+
+window.onload = async (e) => {
+
+    let cases = []
+    let totalCases = 0
+    let totalRecovered = 0
+    let totalActive = 0
+    let totalDeath = 0
+    let country = 'world'
+
+    let getGlobalCases = () => {
+        // document.querySelector('#loading').innerHTML = 'loading'
+        return fetch(baseUrl + '/cases').then((response) => {
+            return response.json()
+        }).then((res) => {
+            document.getElementById('loading').style.display = 'none'
+            // document.querySelector('#loading').innerHTML = ''
+            cases = res
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    let activeCases = (country) => {
+        return fetch(baseUrl + '/'+ country + '/active').then((response) => {
+            return response.json()
+        }).then((res) => {
+            document.getElementById('loading').style.display = 'none'
+            totalActive = res
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    let recoveredCases = (country) => {
+        return fetch(baseUrl + '/' + country + '/recovered').then((response) => {
+            return response.json()
+        }).then((res) => {
+            document.getElementById('loading').style.display = 'none'
+            totalRecovered = res
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    let deathCases = (country) => {
+        return fetch(baseUrl + '/' + country + '/deaths').then((response) => {
+            return response.json()
+        }).then((res) => {
+            document.getElementById('loading').style.display = 'none'
+            totalDeath = res
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    let countryTotalCases = (country) => {
+        return fetch(baseUrl + '/' + country + '/cases').then((response) => {
+            return response.json()
+        }).then((res) => {
+            document.getElementById('loading').style.display = 'none'
+            totalCases = res
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    await getGlobalCases()
+    await activeCases(country)
+    await recoveredCases(country)
+    await deathCases(country)
+    await countryTotalCases(country)
+
+    document.querySelector('#total-cases').innerHTML = totalCases
+    document.querySelector('#total-recovered').innerHTML = totalRecovered
+    document.querySelector('#total-active').innerHTML = totalActive
+    document.querySelector('#total-deaths').innerHTML = totalDeath
+
+    let tbody = document.querySelector('#tbd');
+    for (let data of cases) {
+        let tr = tbody.appendChild(document.createElement('tr'));
+        tr.setAttribute('onclick', `getCountry('${data.country}')`)
+        tr.appendChild(document.createElement('td')).innerHTML = data.country
+        tr.appendChild(document.createElement('td')).innerHTML = data.active
+        tr.appendChild(document.createElement('td')).innerHTML = data.recovered
+        tr.appendChild(document.createElement('td')).innerHTML = data.deaths
+        tr.appendChild(document.createElement('td')).innerHTML = data.cases
+    }
+}
+
+function getCountry(country) {
+    fetch(baseUrl + '/cases/'+ country).then((response) => {
+        return response.json()
+    }).then((res) => {
+        console.log(res)
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+}
